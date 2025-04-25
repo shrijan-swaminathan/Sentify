cdfdfe63bdc451923864b302707fb2dd752a7b88
parvk11 <kparv01@gmail.com>
2025-04-25 00:53:56 -0400

    added ptm's to huggingface for intent and audience

--- sentiment_model.py
+++ sentiment_model.py
@@ -2,6 +2,7 @@ import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from transformers import pipeline
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
 # Download VADER lexicon if not already downloaded
@@ -39,8 +40,8 @@ def analyze_intent(text):
     # result = classifier(text, labels)
     # intent = result['labels'][0]
     # return intent
-    model = DistilBertForSequenceClassification.from_pretrained("intent_classification_model")
-    tokenizer = DistilBertTokenizer.from_pretrained("intent_classification_model")
+    tokenizer = AutoTokenizer.from_pretrained("parvk11/intent_classification_model")
+    model = AutoModelForSequenceClassification.from_pretrained("parvk11/intent_classification_model")
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
     outputs = model(**inputs)
     probs = torch.nn.functional.softmax(outputs.logits, dim=1)
@@ -55,11 +56,19 @@ def analyze_formality(text):
     formality = result['labels'][0]
     return formality
 def analyze_audience(text):
-    classifier = pipeline("zero-shot-classification")
-    labels = ["professional", "personal", "general"]
-    result = classifier(text, labels)
-    audience = result['labels'][0]
-    return audience
+    # classifier = pipeline("zero-shot-classification")
+    # labels = ["professional", "personal", "general"]
+    # result = classifier(text, labels)
+    # audience = result['labels'][0]
+    # return audience
+    tokenizer = AutoTokenizer.from_pretrained("parvk11/audience_classifier_model")
+    model = AutoModelForSequenceClassification.from_pretrained("parvk11/audience_classifier_model")
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+    outputs = model(**inputs)
+    probs = torch.nn.functional.softmax(outputs.logits, dim=1)
+    pred = torch.argmax(probs, dim=1).item()
+    reverse_label_map = {0: 'professional', 1: 'personal', 2: 'general'}
+    return reverse_label_map[pred]
 if __name__ == "__main__":
     # Example usage
     email_text = "I am very happy with the service."
